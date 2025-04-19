014f710bd9f83d31a349f31dbf39494c501a670b
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-04-19 13:15:06 +0200

    Update user.rs

--- src/user.rs
+++ src/user.rs
@@ -1,1 +1,24 @@
+use crate::contribution::Contribution;
+
+pub struct User {
+    pub username: String,
+    pub email: String,
+    pub contributions: Vec<Contribution>,
+}
+
+impl User {
+    pub fn add_contribution(&mut self, contribution: Contribution) {
+        self.contributions.push(contribution);
+    }
+
+    pub fn view_contributions(&self) {
+        for c in &self.contributions {
+            c.display_summary();
+        }
+    }
+
+    pub fn update_profile(&mut self, new_email: String) {
+        self.email = new_email;
+    }
+}
 
