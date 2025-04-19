7f5d9c5e307d17f3b64c404aececdc8da0cb881d
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-04-19 13:17:16 +0200

    Update repository.rs

--- src/repository.rs
+++ src/repository.rs
@@ -1,1 +1,18 @@
+pub struct Repository {
+    pub repo_name: String,
+    pub description: String,
+    pub owner: String,
+    pub language: String,
+    pub contributors: Vec<String>,
+}
+
+impl Repository {
+    pub fn add_contributors(&mut self, contributor: String) {
+        self.contributors.push(contributor);
+    }
+
+    pub fn get_contributors(&self) -> Vec<String> {
+        self.contributors.clone()
+    }
+}
 
