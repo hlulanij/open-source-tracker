a54343fb445167f075ea3e0a80fc5b0a30cbbfe7
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-04-19 13:16:37 +0200

    Update commit.rs

--- src/commit.rs
+++ src/commit.rs
@@ -1,1 +1,20 @@
+pub struct Commit {
+    pub commit_id: String,
+    pub message: String,
+    pub date: String,
+    pub changes: String,
+}
+
+impl Commit {
+    pub fn get_change_summary(&self) -> String {
+        format!("{} - {}", self.date, self.message)
+    }
+
+    pub fn display_commit_details(&self) {
+        println!(
+            "Commit ID: {}\nMessage: {}\nDate: {}\nChanges: {}",
+            self.commit_id, self.message, self.date, self.changes
+        );
+    }
+}
 
