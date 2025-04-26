0314d58f61839d64772d6c363414f0e67c408a31
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-04-26 04:21:19 +0200

    Update RepositoryFactory.java

--- assignment11/src/repositories/RepositoryFactory.java
+++ assignment11/src/repositories/RepositoryFactory.java
@@ -1,1 +1,19 @@
+package assignment11.src.repositories;
+
+import assignment11.src.repositories.inmemory.InMemoryBookRepository;
+
+/**
+ * Factory for creating repository instances.
+ */
+public class RepositoryFactory {
+
+    public static BookRepository createBookRepository(String storageType) {
+        if (storageType.equalsIgnoreCase("memory")) {
+            return new InMemoryBookRepository();
+        }
+
+        // Placeholder: Add support for other storage types (e.g., "json", "sql") here.
+        throw new UnsupportedOperationException("Storage type not supported: " + storageType);
+    }
+}
 
