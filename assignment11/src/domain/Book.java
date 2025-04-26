4540d0a434c0d8523aa62c864ad2ab763fdc5c93
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-04-26 04:11:57 +0200

    Update Book.java

--- assignment11/src/domain/Book.java
+++ assignment11/src/domain/Book.java
@@ -1,1 +1,39 @@
+package assignment11.src.domain;
+
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{id='" + id + "', title='" + title + "', author='" + author + "'}";
+    }
+}
 
