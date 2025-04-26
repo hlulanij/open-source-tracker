1e07899d59275225609431be43fe374089f880e9
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-04-26 04:17:12 +0200

    Update BookRepository.java

--- assignment11/src/repositories/BookRepository.java
+++ assignment11/src/repositories/BookRepository.java
@@ -1,1 +1,11 @@
+package assignment11.src.repositories;
+
+import assignment11.src.domain.Book;
+
+/**
+ * Specific repository interface for Book entity.
+ */
+public interface BookRepository extends Repository<Book, String> {
+    // You can add Book-specific query methods here if needed.
+}
 
