b2390b01e9939725f456185f63d2ce487a316d7c
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-05-02 18:08:13 +0200

    Update Application.java

--- src/assignment12/Application.java
+++ src/assignment12/Application.java
@@ -1,1 +1,13 @@
+package assignment12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment12Application.class, args);
+    }
+}
 
