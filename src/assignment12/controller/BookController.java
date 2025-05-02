9e0384d7c983c089e838f4fd884ed3bcfa1738e7
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-05-02 18:01:55 +0200

    Update BookController.java

--- src/assignment12/controller/BookController.java
+++ src/assignment12/controller/BookController.java
@@ -1,1 +1,13 @@
+package assignment12.controller;
+
+import assignment12.BookService;
+
+public class BookController {
+
+    private final BookService bookService = new BookService();
+
+    public String addBook(String title, String author) {
+        return bookService.addBook(title, author);
+    }
+}
 
