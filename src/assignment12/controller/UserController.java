c7d24902e3f7aadb018d01be86e08ab9e896adc6
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-05-02 18:00:11 +0200

    Update UserController.java

--- src/assignment12/controller/UserController.java
+++ src/assignment12/controller/UserController.java
@@ -1,1 +1,13 @@
+package assignment12.controller;
+
+import assignment12.UserService;
+
+public class UserController {
+
+    private final UserService userService = new UserService();
+
+    public String createUser(String name) {
+        return userService.createUser(name);
+    }
+}
 
