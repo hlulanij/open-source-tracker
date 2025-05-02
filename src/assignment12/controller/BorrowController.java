c3c51d209503d25c4e4385bc6a828f17ea867f0f
Hlulani Jabulani Mathebula <78862317+hlulanij@users.noreply.github.com>
2025-05-02 18:03:25 +0200

    Update BorrowController.java

--- src/assignment12/controller/BorrowController.java
+++ src/assignment12/controller/BorrowController.java
@@ -1,1 +1,13 @@
+package assignment12.controller;
+
+import assignment12.BorrowService;
+
+public class BorrowController {
+
+    private final BorrowService borrowService = new BorrowService();
+
+    public String borrowBook(String userId, String bookId) {
+        return borrowService.borrowBook(userId, bookId);
+    }
+}
 
